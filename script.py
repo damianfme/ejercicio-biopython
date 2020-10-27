b2404df22f7feac4e9a72e4f0a52e313584fd03c
damianfme <70118361+damianfme@users.noreply.github.com>
2020-10-26 20:07:42 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,32 +1,35 @@
+#creacion de script
 from Bio import SeqIO
-from Bio.SeqFeature import FeatureLocation, SeqFeature
 from Bio.SeqRecord import SeqRecord
 import os
-filename= "/mnt/c/Users/usuario/Desktop/ls_orchid.gbk"
-def summarize_contents(filename):
-	FileList = []
-	File_Extension = []
-	
-	if(File_Extension[1] == ".gbk"):
-		type_file= "genbank"
-	else:
-		type_file= "fasta"
-		pass
-	
-	records = list(SeqIO.parse(filename, type_file))
-	cadena = " "
-	path= os.path.dirname(filename)
-	
-	cadena=("File: " + FileList[1])
-	cadena+= ("\nPath " + path)
-	cadena+= ("\nNum_records= " + str(len(records)))
-	
-	for seq_record in SeqIO.parse(filename, type_file):
-		cadena+= ("\n-ID " + str(seq_record.id))
-		cadena+= ("\nName: " + str(seq_record.name))
-		cadena+= ("\nDescription " + str(seq_record.description))
-	return cadena
 
-	if _name_ == "_main_":
-		r = summarize_contents(filename)
-		print(r)
+filename = os.path.abspath("data/ls_orchid.gbk")
+
+#Creacion de funcion
+def summarize_contents(filename):
+	listaOs = os.path.split(filename)
+	listaExt = os.path.splitext(filename)
+	if (listaExt[1] == ".gbk"):
+		type_file= "genbank"
+	else: 
+		type_file= "fasta"
+	record = list(SeqIO.parse(filename, type_file))
+	#Creacion de diccionario
+	d = {}
+	d['File:'] = listaOs[1]
+	d['Path:'] = listaOs[0]
+	d['Num_records:'] = len(record)
+	#Diccionario con listas
+	d['Names:'] = []
+	d['IDs:'] = []
+	d['Descriptions'] = []
+	#Registro de records
+	for seq_rcd in SeqIO.parse(filename,type_file):
+		d['Names:'].append(seq_rcd.name)
+		d['IDs:'].append(seq_rcd.id)
+		d['Descriptions'].append(seq_rcd.description)
+	return d
+#Imprimir la funcion
+if __name__ == "__main__":
+	resultados = summarize_contents(filename)
+	print(resultados)
